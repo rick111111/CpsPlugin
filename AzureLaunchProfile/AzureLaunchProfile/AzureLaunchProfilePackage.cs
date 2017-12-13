e2228746ef6eb3c171ddf9842d8a22943de50b6b
Fangliang Xue <faxue@microsoft.com>
2017-12-13 10:30:49 -0800

    add package dependency

--- AzureLaunchProfile/AzureLaunchProfile/AzureLaunchProfilePackage.cs
+++ AzureLaunchProfile/AzureLaunchProfile/AzureLaunchProfilePackage.cs
@@ -10,7 +10,7 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.Win32;
 
-namespace AzureLaunchProfile
+namespace Microsoft.VisualStudio.ProductionDebug.DebugProfile
 {
     /// <summary>
     /// This is the class that implements the package exposed by this assembly.
