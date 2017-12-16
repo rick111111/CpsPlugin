9abc91d2271bf22222eb2239764587ade065a8e8
Fangliang Xue <faxue@microsoft.com>
2017-12-15 16:28:28 -0800

    add desktop launch target

---
+++ DesktopProjectDebug/DesktopProjectDebug/DebuggableProjectCfg.cs
@@ -0,0 +1,180 @@
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace DesktopProjectDebug
+{
+    [ComVisible(false)]
+    internal class DebuggableProjectCfg :
+        IVsDebuggableProjectCfg,
+        IVsDebuggableProjectCfg2,
+        IVsQueryDebuggableProjectCfg
+    {
+        private IVsHierarchy _project;
+        private string _exeNameToLaunch;
+        Guid CLSID_ComPlusOnlyDebugEngine4 = new Guid("{FB0D4648-F776-4980-95F8-BB7F36EBC1EE}");
+
+        public DebuggableProjectCfg(IVsHierarchy project, string exeNameToLaunch)
+        {
+            _project = project;
+            _exeNameToLaunch = exeNameToLaunch;
+        }
+
+        public virtual int get_DisplayName(out string name)
+        {
+            name = "Debug";
+            return VSConstants.S_OK;
+        }
+
+        public virtual int get_IsDebugOnly(out int fDebug)
+        {
+            fDebug = 0;
+            return VSConstants.S_OK;
+        }
+
+        public virtual int get_IsReleaseOnly(out int fRelease)
+        {
+            fRelease = 0;
+            return VSConstants.S_OK;
+        }
+
+        public virtual int EnumOutputs(out IVsEnumOutputs eo)
+        {
+            eo = null;
+            return VSConstants.E_NOTIMPL;
+        }
+        public virtual int get_BuildableProjectCfg(out IVsBuildableProjectCfg pb)
+        {
+            pb = null;
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public virtual int get_CanonicalName(out string name)
+        {
+            name = "Debug";
+            return VSConstants.S_OK;
+        }
+
+        public virtual int get_IsPackaged(out int pkgd)
+        {
+            pkgd = 0;
+            return VSConstants.S_OK;
+        }
+
+        public virtual int get_IsSpecifyingOutputSupported(out int f)
+        {
+            f = 0;
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public virtual int get_Platform(out Guid platform)
+        {
+            platform = Guid.Empty;
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public virtual int get_ProjectCfgProvider(out IVsProjectCfgProvider p)
+        {
+            p = null;
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public virtual int get_RootURL(out string root)
+        {
+            root = "";
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public virtual int get_TargetCodePage(out uint target)
+        {
+            target = 0;
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public virtual int get_UpdateSequenceNumber(ULARGE_INTEGER[] li)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public virtual int OpenOutput(string name, out IVsOutput output)
+        {
+            output = null;
+            return VSConstants.E_NOTIMPL;
+        }
+
+        /// <summary>
+        /// IVsQueryDebuggableProjectCfg
+        /// Returns the debug launch targets
+        /// </summary>
+        public int QueryDebugTargets(uint grfLaunch, uint cTargets, VsDebugTargetInfo2[] rgDebugTargetInfo, uint[] cActual)
+        {
+            cActual[0] = 1;
+            if (cTargets == 0)
+            {
+                return VSConstants.E_NOTIMPL;
+            }
+
+            VsDebugTargetInfo2 info = new VsDebugTargetInfo2();
+            info.dlo = (uint)DEBUG_LAUNCH_OPERATION.DLO_CreateProcess;
+            info.guidLaunchDebugEngine = CLSID_ComPlusOnlyDebugEngine4;
+
+            // Note. A bug in the debugger will cause a failure if a PID is set but the exe name
+            // is NULL. So we just blindly set it here.
+            info.bstrExe = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), _exeNameToLaunch);
+            info.bstrArg = null;
+            info.bstrCurDir = null;
+            info.LaunchFlags = grfLaunch;
+            rgDebugTargetInfo[0] = info;
+
+            return VSConstants.E_NOTIMPL;
+        }
+
+        /// <summary>
+        /// IVsDebuggableProjectCfg
+        /// </summary>
+        public int DebugLaunch(uint grfLaunch)
+        {
+            VsDebugTargetInfo4 info = new VsDebugTargetInfo4();
+            info.dlo = (uint)DEBUG_LAUNCH_OPERATION.DLO_CreateProcess;
+            info.guidLaunchDebugEngine = CLSID_ComPlusOnlyDebugEngine4;
+
+            // Note. A bug in the debugger will cause a failure if a PID is set but the exe name
+            // is NULL. So we just blindly set it here.
+            info.bstrExe = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), _exeNameToLaunch);
+            info.bstrArg = null;
+            info.bstrCurDir = null;
+            info.LaunchFlags = grfLaunch;
+
+            IVsDebugger4 debugger = ServiceProvider.GlobalProvider.GetService(typeof(SVsShellDebugger)) as IVsDebugger4;
+            var debugTargetInfos = new VsDebugTargetInfo4[] { info };
+            // Debugger will throw on failure
+            var tpi = new VsDebugTargetProcessInfo[1];
+            debugger.LaunchDebugTargets4(1, debugTargetInfos, tpi);
+            return VSConstants.S_OK;
+        }
+
+        /// <summary>
+        /// IVsDebuggableProjectCfg
+        /// Return whether it is OK to launch the debugger
+        /// </summary>
+        public int QueryDebugLaunch(uint grfLaunch, out int pfCanLaunch)
+        {
+            pfCanLaunch = 1;
+            return VSConstants.S_OK;
+        }
+
+        /// <summary>
+        /// IVsDebuggableProjectCfg2. We override to prevent inner project system from playing with
+        /// their hosting process
+        /// </summary>
+        public int OnBeforeDebugLaunch(uint grfLaunch)
+        {
+            return VSConstants.S_OK;
+        }
+    }
+}
---
+++ DesktopProjectDebug/DesktopProjectDebug/PropertyPage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio;
+using System.Runtime.InteropServices;
+
+namespace DesktopProjectDebug
+{
+    [Guid("3F1E4810-F43A-47EF-8294-7978848C45B3")]
+    public partial class PropertyPage : UserControl, IPropertyPage
+    {
+        public PropertyPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPageSite(IPropertyPageSite pPageSite)
+        {
+        }
+
+        public void Activate(IntPtr hWndParent, RECT[] pRect, int bModal)
+        {
+            Win32Methods.SetParent(this.Handle, hWndParent);
+        }
+
+        public void Deactivate()
+        {
+        }
+
+        public void GetPageInfo(PROPPAGEINFO[] pPageInfo)
+        {
+            PROPPAGEINFO info = new PROPPAGEINFO();
+
+            info.cb = (uint)Marshal.SizeOf(typeof(PROPPAGEINFO));
+            info.dwHelpContext = 0;
+            info.pszDocString = null;
+            info.pszHelpFile = null;
+            info.pszTitle = "Desktop Project Debugger";
+            info.SIZE.cx = this.Size.Width;
+            info.SIZE.cy = this.Size.Height;
+            if (pPageInfo != null && pPageInfo.Length > 0)
+                pPageInfo[0] = info;
+        }
+
+        public void SetObjects(uint cObjects, object[] ppunk)
+        {
+        }
+
+        public void Show(uint nCmdShow)
+        {
+            if (nCmdShow != SW_HIDE)
+                this.Show();
+            else
+                this.Hide();
+        }
+
+        private const int SW_HIDE = 0;
+
+        void IPropertyPage.Move(RECT[] pRect)
+        {
+            if (pRect == null || pRect.Length <= 0)
+                throw new ArgumentNullException("pRect");
+
+            Microsoft.VisualStudio.OLE.Interop.RECT r = pRect[0];
+
+            this.Location = new Point(r.left, r.top);
+        }
+
+        public int IsPageDirty()
+        {
+            return VSConstants.S_FALSE;
+        }
+
+        public int Apply()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Help(string pszHelpDir)
+        {
+        }
+
+        public int TranslateAccelerator(MSG[] pMsg)
+        {
+            return 0;
+        }
+    }
+}
